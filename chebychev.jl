f23e6d73c201563e13f9bd3df04516e46f9f31cd
RyanOrsinger <ryan.orsinger@gmail.com>
2020-12-23 14:58:19 -0600

    my chebychev function needs some work

--- chebychev.jl
+++ chebychev.jl
@@ -57,19 +57,24 @@ avg(ones(100)), rms(ones(100))
 std(ones(100))
 
 # ╔═╡ 331ba3aa-3f50-11eb-3e9c-198a851b584e
-function get_chebychev(x, a)
+function get_cheby(x, a)
 	"""
-	returns the Chebychev bound and the number of entries of the vector with abs() >= a
+	returns the number of entries of the vector with abs() >= a
 	"""
 	
 	# Define the Chebychev bound function
-	cb = cheb_bound(x, a) = floor(norm(x)^2/a)
 	
 	# number of entries of x with abs(x_i) >= a
 	num_entries = sum(abs.(x) .>= a)
 	
-	return cb, num_entries
+	return num_entries
 end
+
+# ╔═╡ ad673dc4-40cf-11eb-0fa7-dbbfc53f66c2
+a = [1, 2, 3, 4, 5, 6, 7]
+
+# ╔═╡ b736b872-40cf-11eb-34a4-59f1fd01fe7c
+get_cheby(a, 3)	
 
 # ╔═╡ 7fbf7060-3f50-11eb-0fba-2931fcc14fb3
 md"""
@@ -95,6 +100,44 @@ get_chebychev(a, x)
 # ╔═╡ ef9ef650-3f53-11eb-1fba-23f2eaf92191
 x
 
+# ╔═╡ 6dadb310-40d2-11eb-1491-9d699b1914ea
+
+
+# ╔═╡ 6d992556-40d2-11eb-2127-79898f0d4009
+
+
+# ╔═╡ 6d7da0ec-40d2-11eb-3138-b525bface9a5
+# chebyshev  inequality to get number of entries <= a
+function number_of_entries_greater_than_equal_to(x, a)
+	"""
+	returns the number of entries of the vector with abs() >= a
+	"""
+		
+	# number of entries of x with abs(x_i) >= a
+	num_entries = sum(abs.(x) .>= a)
+	
+	return num_entries
+end
+
+# ╔═╡ 6d61a1d0-40d2-11eb-0329-ddda546a2e19
+number_of_entries_greater_than_equal_to([0, 0, 0, 0, 5], 3.5)
+
+# ╔═╡ 6d17cc68-40d2-11eb-2f28-c306f46a1dbe
+number_of_entries_greater_than_equal_to([1, 2, 3, 4, 5], 0)
+
+# ╔═╡ 21b7f530-40d3-11eb-07c9-d70dd92c224b
+floor(norm([0, 0, 0, 0, 5])^2/3.5)
+
+# ╔═╡ f16a5c4c-40cf-11eb-3c49-45ed03ba2b3f
+function cheb_bound(x, a) 
+	return floor(norm(x)^2/a)
+end
+
+# ╔═╡ 1c74c8be-40d3-11eb-114b-693d0807d79e
+function cheb_bound(x,a)
+	return floor(norm(x)^2/a)
+end
+
 # ╔═╡ Cell order:
 # ╠═e2d3d3d8-3f43-11eb-02de-1d42b03957ef
 # ╠═8a061a1a-3f46-11eb-16bc-1195129fbe8b
@@ -110,9 +153,19 @@ x
 # ╠═912adb0c-3f5f-11eb-1542-09650e2cc8f2
 # ╠═a5c5ec9e-3f5f-11eb-2e10-87989fa9f511
 # ╠═b3fcf7c6-3f5f-11eb-1256-c1a05a53e673
+# ╠═f16a5c4c-40cf-11eb-3c49-45ed03ba2b3f
 # ╠═331ba3aa-3f50-11eb-3e9c-198a851b584e
+# ╠═ad673dc4-40cf-11eb-0fa7-dbbfc53f66c2
+# ╠═b736b872-40cf-11eb-34a4-59f1fd01fe7c
 # ╠═7fbf7060-3f50-11eb-0fba-2931fcc14fb3
 # ╠═2651d4b6-3f53-11eb-3fa0-eff9f54beaf3
 # ╠═9e0ee490-3f52-11eb-2c2b-3b615036074c
 # ╠═9e36d2de-3f52-11eb-0942-4ffd9515bc09
 # ╠═ef9ef650-3f53-11eb-1fba-23f2eaf92191
+# ╠═6dadb310-40d2-11eb-1491-9d699b1914ea
+# ╠═6d992556-40d2-11eb-2127-79898f0d4009
+# ╠═6d7da0ec-40d2-11eb-3138-b525bface9a5
+# ╠═6d61a1d0-40d2-11eb-0329-ddda546a2e19
+# ╠═6d17cc68-40d2-11eb-2f28-c306f46a1dbe
+# ╠═1c74c8be-40d3-11eb-114b-693d0807d79e
+# ╠═21b7f530-40d3-11eb-07c9-d70dd92c224b
